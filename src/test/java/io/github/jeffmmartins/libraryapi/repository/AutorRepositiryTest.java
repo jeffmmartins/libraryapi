f744d828ca1efc03c854a088af88494ae3211e28
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-09-02 17:45:48 -0300

    Feat: metodos de teste atualizar e slavar criados

--- src/test/java/io/github/jeffmmartins/libraryapi/repository/AutorRepositiryTest.java
+++ src/test/java/io/github/jeffmmartins/libraryapi/repository/AutorRepositiryTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
+import java.util.Optional;
+import java.util.UUID;
 
 @SpringBootTest
 public class AutorRepositiryTest {
@@ -16,11 +18,32 @@ public class AutorRepositiryTest {
     @Test
     public void salvarTest(){
         Autor autor = new Autor();
-        autor.setNome("Jose");
+        autor.setNome("Maria");
         autor.setNacionalidade("Brasileiro");
         autor.setDataNascimento(LocalDate.of(1950,1, 31));
 
         var autorSalvo = repository.save(autor);
         System.out.println("Autor Salvo: " + autorSalvo);
+    }
+
+    @Test
+    public void atualizarTest(){
+        //transformando uma string em UUID
+        var id = UUID.fromString("a76c3a09-29c8-48ee-8443-51521a45dcca");
+
+        Optional<Autor> possivelAutor = repository.findById(id);
+
+        if (possivelAutor.isPresent()){
+            Autor autorEncontrado = possivelAutor.get() ;
+            System.out.println("Dados do Autor: ");
+            //retorna o objeto que está la dentro
+            System.out.println(autorEncontrado);
+
+            autorEncontrado.setDataNascimento(LocalDate.of(1960,1,30));
+            //esse metodo tanto salva como atualiza
+            repository.save(autorEncontrado);
+        }
+
+
     }
 }
