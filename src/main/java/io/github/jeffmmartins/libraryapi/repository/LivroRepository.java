45ddfe0805b30ccad8d5b2405fffa6146f38c62a
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-09-12 18:45:53 -0300

    Feat: query deleteByGenero criado

--- src/main/java/io/github/jeffmmartins/libraryapi/repository/LivroRepository.java
+++ src/main/java/io/github/jeffmmartins/libraryapi/repository/LivroRepository.java
@@ -4,9 +4,11 @@ import io.github.jeffmmartins.libraryapi.model.Autor;
 import io.github.jeffmmartins.libraryapi.model.GeneroLivro;
 import io.github.jeffmmartins.libraryapi.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -55,4 +57,9 @@ public interface LivroRepository extends JpaRepository<Livro, UUID> {
 
     @Query("select l from Livro l where l.genero = :genero order by :paramOrdenacao")
     List<Livro> findByGenero(@Param("genero") GeneroLivro generLivro, @Param("paramOrdenacao") String nomePropriedade);
+
+    @Modifying
+    @Transactional
+    @Query("delete from Livro where genero = ?1")
+    void deleteByGenero(GeneroLivro genero);
 }
