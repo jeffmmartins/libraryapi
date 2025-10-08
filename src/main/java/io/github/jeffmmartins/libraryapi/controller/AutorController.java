2fe017004bf2cdde23d01c7d1d8e6f8ee33e5269
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2025-10-08 19:08:42 -0300

    Feat: classe AutorService criada

--- src/main/java/io/github/jeffmmartins/libraryapi/controller/AutorController.java
+++ src/main/java/io/github/jeffmmartins/libraryapi/controller/AutorController.java
@@ -1,6 +1,8 @@
 package io.github.jeffmmartins.libraryapi.controller;
 
 import io.github.jeffmmartins.libraryapi.controller.dto.AutorDTO;
+import io.github.jeffmmartins.libraryapi.model.Autor;
+import io.github.jeffmmartins.libraryapi.service.AutorService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,9 +12,17 @@ import org.springframework.web.bind.annotation.*;
 // http://localhost:8080/autores
 public class AutorController {
 
+    private final AutorService autorService;
+
+    public AutorController(AutorService autorService) {
+        this.autorService = autorService;
+    }
+
     @PostMapping
     // quando receber json para /autores vai mapear para o objeto AutorDTO
     public ResponseEntity salvar(@RequestBody AutorDTO autor){
+        Autor autorEntidade = autor.mapearParaAutor();
+        autorService.salvar(autorEntidade);
         return new ResponseEntity("Autor salvo com sucesso" + autor, HttpStatus.CREATED);
     }
 }
--- src/main/java/io/github/jeffmmartins/libraryapi/controller/dto/AutorDTO.java
+++ src/main/java/io/github/jeffmmartins/libraryapi/controller/dto/AutorDTO.java
@@ -1,8 +1,18 @@
 package io.github.jeffmmartins.libraryapi.controller.dto;
 
+import io.github.jeffmmartins.libraryapi.model.Autor;
+
 import java.time.LocalDate;
 
 //Classe imutavel, não é possivel mudar os valores
 public record AutorDTO(String nome, LocalDate dataNascimento, String nacionalidade) {
 
+    //Esse metodo cria o autor e preenche os dados que vem no DTO
+    public Autor mapearParaAutor(){
+        Autor autor = new Autor();
+        autor.setNome(this.nome);
+        autor.setDataNascimento(this.dataNascimento);
+        autor.setNacionalidade(this.nacionalidade);
+        return autor;
+    }
 }
